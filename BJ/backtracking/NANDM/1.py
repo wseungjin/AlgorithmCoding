366217ef88fd89127df312b613144de31227d1a3
wseungjin <bose1021@naver.com>
2020-07-06 22:12:13 +0900

    name KR TO ENG

---
+++ BJ/backtracking/NANDM/1.py
@@ -0,0 +1,18 @@
+# f = open("input.txt", 'r')
+# line = f.readline()
+line = input()
+
+output=line.split(" ")
+
+N=int(output[0])
+M=int(output[1])
+
+array=[]
+
+for i in range(N):
+    array[i]=i+1 
+    
+    
+
+
+
